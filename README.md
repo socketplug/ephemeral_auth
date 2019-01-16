33e521af0948c1238f887437702ee6451756b8c3
chip reed <chip@chip.sh>
2019-01-16 09:51:43 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ephemeral_auth
+
+A self contained 3rd party auth server for plug.dj
---
+++ src/main.rs
@@ -0,0 +1,29 @@
+/*
+ * ephemeral_auth: A self contained 3rd party auth server for plug.dj
+ * Copyright (C) 2019 Chip Reed
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(1 + 1, 2);
+    }
+}
